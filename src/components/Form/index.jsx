89222caa90980941ed8d72e76a1d9dc570e8893c
Ladata Studio <ladata.studio@gmail.com>
2022-12-21 08:34:34 +0100

    Form with Formik & Yup Validation

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
 
-const FormComponent = ({ handleSuccess }) => {
+const FormComponent = ({ onSubmit }) => {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Required field'),
+    email: Yup.string()
+      .email('Must be a valid e-mail address')
+      .required('Required field'),
+  });
+
   return (
-    <form onSubmit={handleSuccess}>
-      <label for="name">Name:</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        autoComplete="off"
-        required
-      />
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+      }}
+      onSubmit={onSubmit}
+      validationSchema={schema}
+    >
+      {({errors, touched}) => (
+        <Form>
+          <label for="name">Name:</label>
+          <Field
+            type="text"
+            name="name"
+            id="name"
+            autoComplete="off"
+          />
+          {touched.name && errors && <div>{errors.name}</div>}
 
-      <label for="email">E-mail:</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        autoComplete="off"
-        required
-      />
+          <label for="email">E-mail:</label>
+          <Field
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="off"
+          />
+          {touched.email && errors && (
+            <div>{errors.email}</div>
+          )}
 
-      <input
-        type="submit"
-        value="Submit"
-      />
-    </form>
+          <Field
+            type="submit"
+            value="Submit"
+          />
+        </Form>
+      )}
+    </Formik>
   );
 };
 
